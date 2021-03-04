74959999c62a25c66b6bfd415cd97c3204daef85
ChulinNakazawa <102908824+ChulinNakazawa@users.noreply.github.com>
2021-03-04 09:47:55 +0000

    tweaking commit

--- home/.config/doom/themes/my-doom-one-theme.el
+++ home/.config/doom/themes/my-doom-one-theme.el
@@ -32,4 +32,17 @@ determine the exact padding."
 (def-doom-theme my-doom-one
   "A dark theme inspired by Atom One Dark"
 
-  
+  ;; name        default   256       16
+  ((bg         '("#000000" nil       nil            ))
+   (bg-alt     '("#21242b" nil       nil            ))
+   (base0      '("#1B2229" "black"   "black"        ))
+   (base1      '("#1c1f24" "#1e1e1e" "brightblack"  ))
+   (base2      '("#202328" "#2e2e2e" "brightblack"  ))
+   (base3      '("#23272e" "#262626" "brightblack"  ))
+   (base4      '("#3f444a" "#3f3f3f" "brightblack"  ))
+   (base5      '("#5B6268" "#525252" "brightblack"  ))
+   (base6      '("#73797e" "#6b6b6b" "brightblack"  ))
+   (base7      '("#9ca0a4" "#979797" "brightblack"  ))
+   (base8      '("#DFDFDF" "#dfdfdf" "white"        ))
+   (fg         '("#bbc2cf" "#bfbfbf" "brightwhite"  ))
+   (fg-alt     '("#5B6268" "#2d2d2d" "white"        )
