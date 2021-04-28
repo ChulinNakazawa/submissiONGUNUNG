5a45bf65c39ca78690284a3be7056bbf033be8c9
ChulinNakazawa <102908824+ChulinNakazawa@users.noreply.github.com>
2021-04-28 06:35:18 +0000

    Change index.html

---
+++ home/.config/nvim/init.lua
@@ -0,0 +1,12 @@
+local cmd = vim.cmd
+local fn = vim.fn
+local g = vim.g
+local o = vim.o
+
+g.mapleader = ' '
+
+o.softtabstop = 2
+o.shiftwidth = 2
+o.expandtab = true
+o.number = true
+o.termguicolors =
