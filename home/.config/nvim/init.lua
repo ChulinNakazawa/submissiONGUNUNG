e57d96588cdcc2ada3bfb0967ede3f10da141521
ChulinNakazawa <102908824+ChulinNakazawa@users.noreply.github.com>
2021-04-28 13:31:06 +0000

    Expand README.md

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -50,4 +50,27 @@ function prequire(...)
 end
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) 
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  spec = {
+    'folke/tokyonight.nvim',
+
+    'neoclide/coc.nvim',
+    'junegunn/fzf',
+    'junegunn/fzf.vim',
+    'lewis6991/gitsigns.nvim',
+    'phaazon/hop.nvim',
+    'rluba/jai.vim',
+    'kdheepak/lazygit.nvim',
+    'ggandor/lightspeed.nvim',
+    'alaviss/nim.nvim',
+    {'hrsh7th/nvim-cmp', dependencies = {'hrsh7th/cmp-buffer', 'hrsh7th/cmp-nvim-lsp'}},
+    'terrortylor/nvim-comment',
+    'mfussenegger/nvim-dap',
+    'neovim/nvim-lspconfig',
+    {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+    {'romgrk/nvim-treesitt
