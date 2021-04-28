ead458cde56626fdebe8aca03ab10d15722427e6
ChulinNakazawa <102908824+ChulinNakazawa@users.noreply.github.com>
2021-04-28 09:25:11 +0000

    spelling errors

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -9,4 +9,25 @@ o.softtabstop = 2
 o.shiftwidth = 2
 o.expandtab = true
 o.number = true
-o.termguicolors =
+o.termguicolors = true
+
+o.backup = true
+o.backupdir = os.getenv('HOME') .. '/.cache/nvim/backup//'
+o.directory = os.getenv('HOME') .. '/.cache/nvim/swap//'
+o.undofile = true
+o.undodir = os.getenv('HOME') .. '/.cache/nvim/undo//'
+o.updatetime = 100
+
+cmd 'filetype plugin on'
+cmd 'filetype plugin indent on'
+
+local stl = {
+  '%#ColorColumn#%2f',          -- buffer number
+  ' ',                          -- separator
+  '%<',                         -- truncate here
+  '%*»',                        -- separator
+  '%*»',                        -- separator
+  '%#DiffText#%m',              -- modified flag
+  '%r',                         -- readonly flag
+  '%*»',                        -- separator
+  '%#CursorLine#(%l/%L,%c)%*»', -- line no./no. of lines,col n
