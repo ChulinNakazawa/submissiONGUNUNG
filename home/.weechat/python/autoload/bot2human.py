4a5d4aab7e21d1073744b9d5be75e68f77c207b2
ChulinNakazawa <102908824+ChulinNakazawa@users.noreply.github.com>
2021-07-02 14:11:55 +0000

    New translations default.json (Portuguese)

--- home/.weechat/python/autoload/bot2human.py
+++ home/.weechat/python/autoload/bot2human.py
@@ -15,4 +15,27 @@
 #       space seperated nicknames to forwarding bots
 #       example: teleboto toxsync tg2arch
 #
-#   plugins.var.python
+#   plugins.var.python.nick_content_re.X
+#       X is a 0-2 number. This options specifies regex to match nickname
+#       and content. Default regexes are r'\[(?P<nick>.+?)\] (?P<text>.*)',
+#       r'\((?P<nick>.+?)\) (?P<text>.*)', and r'<(?P<nick>.+?)> (?P<text>.*)'
+#
+#   plugins.var.python.nick_re_count
+#       Number of rules defined
+#
+
+# Changelog:
+# 0.3.0: Add relayed nicks into nicklist, enabling completion
+# 0.2.2: Support ZNC timestamp
+# 0.2.1: Color filtering only applies on nicknames
+#        More than 3 nick rules can be defined
+# 0.2.0: Filter mIRC color and other control seq from message
+# 0.1.1: Bug Fixes
+# 0.1: Initial Release
+#
+
+import weechat as w
+import re
+
+SCRIPT_NAME = "bot2human"
+SCRIPT_AUTHOR =
