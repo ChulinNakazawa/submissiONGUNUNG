0b2d313549a258a07eb6fe4282e876bf3e691f41
ChulinNakazawa <102908824+ChulinNakazawa@users.noreply.github.com>
2021-07-30 22:41:10 +0000

    more samples

---
+++ home/bin/suid.mk
@@ -0,0 +1,15 @@
+
+P := ip route ifconfig wpa_gui ss brctl
+export CFLAGS :=
+
+define create
+.PHONY: $1
+$1: suid.cc
+	$(LINK.c) $$^ -o $$@
+	sudo chown root: $$@
+	sudo chmod u+s $$@
+endef
+
+all: $P
+
+$(foreach i,$P,$(eval $(call create,$i)))
