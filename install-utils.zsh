2429ae04a067aa84bd7db8f084df01859519f18b
ChulinNakazawa <102908824+ChulinNakazawa@users.noreply.github.com>
2023-02-07 15:52:41 +0000

    some polishing

--- install-utils.zsh
+++ install-utils.zsh
@@ -50,4 +50,37 @@ clone() {
     [[ -d $2 ]] || git clone $1 $2
 }
 
-clone https://gi
+clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+mkdir -p ~/Dev/Bin
+clone https://github.com/hugsy/gef ~/Dev/Bin/gef
+clone https://github.com/zachriggle/pwndbg ~/Dev/Bin/pwndbg
+clone https://github.com/Gallopsled/pwntools ~/Dev/Bin/pwntools
+
+# install
+
+install_system_pkgs() {
+  info apt-get install ${(v)ubuntu_pkg}
+  read
+}
+
+install_haskell() {
+  # stack
+  curl -sSL https://get.haskellstack.org/ | sh
+
+  # structred-haskell-mode
+  stack install structured-haskell-mode
+
+  # intero
+  # git clone https://github.com/commercialhaskell/intero
+  # cd intero && stack build && stack install
+}
+
+preexec() {
+  echo "\e[1;33m== $2\e[m"
+}
+
+((${1+1})) && "$@"
+
+#install_stack
+#install_system_pkgs
